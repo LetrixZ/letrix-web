bf0534af1e835bc0916d7d42e07afb07b6111f69
Fermin Cirella <fermincirella.ar@gmail.com>
2023-10-17 00:02:19 -0300

    Init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<div class="container mx-auto text-center my-8 flex flex-col gap-y-4">
+	<img class="mx-auto w-48 rounded-full" src="osaka.webp" alt="Ayumu Kasuga" />
+	<div class="space-y-1">
+		<h1 class="h1">My cool website</h1>
+		<h3 class="h3 font-light">Super cool website</h3>
+	</div>
+</div>
